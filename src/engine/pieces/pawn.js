cf3b7d0aa9448baa880d0c614d08289ccde87527
Alison Thomson <Alison.Thomson@softwire.com>
2022-05-03 14:34:07 +0100

    Pawn cannot move if blocked

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -15,13 +15,21 @@ export default class Pawn extends Piece {
             if(location.row === 1) {
                 availableMoves.push(Square.at(location.row + 2, location.col))
             }
-
         } else {
             availableMoves.push(Square.at(location.row - 1, location.col))
             if(location.row === 6) {
                 availableMoves.push(Square.at(location.row - 2, location.col))
             }
-
+        }
+        //for each square in availableMoves
+        for (let i = 0; i <= availableMoves.length - 1; i++) {
+            if (board.getPiece(availableMoves[i])) {
+                if (i === 0){
+                    availableMoves = [];
+                } else {
+                    availableMoves.splice(i, 1);
+                }
+            }
         }
         return availableMoves;
     }
